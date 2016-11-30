465733d414981015b70ae91142f5e763f990cd16
Douglas Orr <douglas.orr99@gmail.com>
2016-11-30 22:26:51 +0000

    WIP tweak Avro schema writing, for better compatibility

--- photonai/__main__.py
+++ photonai/__main__.py
@@ -32,6 +32,42 @@ def _stop_condition(nbots, time_limit):
         )
 
 
+def _replace_key(d, key, value):
+    d = d.copy()
+    d[key] = value
+    return d
+
+
+def _dedupe_schema(schema, names):
+    if type(schema) == list:
+        # enum
+        return [_dedupe_schema(e, names) for e in schema]
+    elif type(schema) == dict:
+        if schema['type'] == 'array':
+            return _replace_key(schema, 'items',
+                                _dedupe_schema(schema['items'], names))
+        elif schema['type'] == 'map':
+            return _replace_key(schema, 'values',
+                                _dedupe_schema(schema['values'], names))
+        elif schema['type'] == 'record':
+            # only support qualified names everywhere - simpler!
+            name = '%s.%s' % (schema['namespace'], schema['name'])
+            if name in names:
+                return name
+            else:
+                schema = schema.copy()
+                schema['fields'] = [
+                    _replace_key(
+                        f, 'type', _dedupe_schema(f['type'], names))
+                    for f in schema['fields']]
+                names[name] = schema
+                return schema
+        else:
+            raise ValueError('Unexpected type %s' % schema['type'])
+    else:
+        return schema
+
+
 @click.command()
 @click.argument('bots', nargs=-1, type=click.Path(dir_okay=False, exists=True))
 @click.argument('out', type=click.Path(writable=True))
@@ -50,6 +86,16 @@ def _stop_condition(nbots, time_limit):
 def run(bots, out, map, step_duration, seed, force, repeat_bots, time_limit):
     '''Run a single competitive game with some bots, and save the log.
     '''
+    import copy
+    # foo = copy.deepcopy(schema.STEP)
+    # repo = fastavro._schema.SCHEMA_DEFS.copy()
+    # fastavro._schema.populate_schema_defs(foo, repo)
+    print("------------------------------------------------------------")
+    print(schema.STEP)
+    print("------------------------------------------------------------")
+    print(_dedupe_schema(schema.STEP, {}))
+    print("------------------------------------------------------------")
+
     if (not force) and os.path.exists(out):
         raise click.ClickException(
             'Output file "%s" already exists - delete to proceed' % out)
@@ -69,7 +115,9 @@ def run(bots, out, map, step_duration, seed, force, repeat_bots, time_limit):
 
     with open(out, 'wb') as f:
         try:
-            writer = fastavro.writer.Writer(f, schema.STEP, codec='deflate')
+            writer = fastavro.writer.Writer(f,
+                                            _dedupe_schema(schema.STEP, {}),
+                                            codec='deflate')
             for step in steps:
                 writer.write(step)
         except game.Stop as stop:
