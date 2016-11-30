05ea1d4bba4082fbfb679e516926956dc009fa31
Douglas Orr <douglas.orr99@gmail.com>
2016-11-30 23:05:08 +0000

    Enable the cli runner to spit out jsonlines (for ease of use by the web player) (closes #3)

--- photonai/__main__.py
+++ photonai/__main__.py
@@ -32,9 +32,41 @@ def _stop_condition(nbots, time_limit):
         )
 
 
+def _write_avro(out, data, schema_):
+    '''Write an avro file, careful to flush after terminating with an exception.
+    '''
+    with open(out, 'wb') as f:
+        writer = fastavro.writer.Writer(f, schema_, codec='deflate')
+        try:
+            for datum in data:
+                writer.write(datum)
+        finally:
+            writer.flush()
+
+
+def _write_json(out, data):
+    '''Write a jsonlines file.
+    '''
+    with open(out, 'w') as f:
+        for datum in data:
+            f.write(json.dumps(datum) + '\n')
+
+
+def _infer_format(path):
+    '''Try to guess if we're writing an Avro file or a jsonlines file
+    (eagerly falls back to jsonlines).
+    '''
+    if path.endswith('avro'):
+        return 'avro'
+    else:
+        return 'json'
+
+
 @click.command()
 @click.argument('bots', nargs=-1, type=click.Path(dir_okay=False, exists=True))
 @click.argument('out', type=click.Path(writable=True))
+@click.option('--format', type=click.Choice(('json', 'avro')), default=None,
+              help='output file format')
 @click.option('-m', '--map', type=click.STRING, default='singleton',
               show_default=True, help='name of a photonai.maps map to use')
 @click.option('-s', '--step-duration', type=click.FLOAT, default=0.01,
@@ -47,12 +79,15 @@ def _stop_condition(nbots, time_limit):
               help='number of times to repeat the botlist')
 @click.option('--time-limit', default=60.0,
               help='hard limit on the simulation time for a draw')
-def run(bots, out, map, step_duration, seed, force, repeat_bots, time_limit):
+def run(bots, out, format, map,
+        step_duration, seed, force, repeat_bots, time_limit):
     '''Run a single competitive game with some bots, and save the log.
     '''
     if (not force) and os.path.exists(out):
         raise click.ClickException(
             'Output file "%s" already exists - delete to proceed' % out)
+
+    format = format or _infer_format(out)
 
     random = np.random.RandomState(seed)
     map = getattr(maps, map).Map(random.randint(2 ** 32))
@@ -67,18 +102,19 @@ def run(bots, out, map, step_duration, seed, force, repeat_bots, time_limit):
         stop=_stop_condition(len(bots), time_limit),
         step_duration=step_duration)
 
-    with open(out, 'wb') as f:
-        try:
-            writer = fastavro.writer.Writer(f, schema.STEP, codec='deflate')
-            for step in steps:
-                writer.write(step)
-        except game.Stop as stop:
-            sys.stderr.write('%s\n' % stop)
-            print(json.dumps(None
-                             if stop.winner is None else
-                             stop.winner['name']))
-        finally:
-            writer.flush()
+    try:
+        if format == 'avro':
+            _write_avro(out, steps, schema.STEP)
+        elif format == 'json':
+            _write_json(out, steps)
+        else:
+            raise click.ArgumentException(
+                'Unknown format "%s" (expected {json avro})' % format)
+    except game.Stop as stop:
+        sys.stderr.write('%s\n' % stop)
+        click.echo(json.dumps(None
+                              if stop.winner is None else
+                              stop.winner['name']))
 
 
 sys.argv[0] = 'photonai'
