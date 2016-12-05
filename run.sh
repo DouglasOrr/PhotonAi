38db5fedc76b0b3be1a4604ed810e452206a3c8c
Douglas Orr <douglas.orr99@gmail.com>
2016-12-05 17:10:02 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 docker run --rm -it \
        -v `pwd`:/local -v /var/run/docker.sock:/var/run/docker.sock \
        -w /local -e HOST_ROOT=`pwd` \
-       photonai photonai run $@
+       douglasorr/photonai photonai run $@
