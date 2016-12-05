8adf5de7c87bca97f1712910daab1e296badfe20
Douglas Orr <douglas.orr99@gmail.com>
2016-12-05 17:10:31 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='photonai',
-    version='0.1',
+    version='0.2',
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
